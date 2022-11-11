35ea58dbe6a1e73039c1c4d4e919099bf9b15856
ABHINAV <abhinavg1000@gmail.com>
2022-11-12 04:00:37 +0530

    Important Information

--- src/main/java/com/test/SaveObjectTest.java
+++ src/main/java/com/test/SaveObjectTest.java
@@ -9,17 +9,23 @@ import org.hibernate.cfg.Configuration;
 public class SaveObjectTest {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
+        configuration.configure("hibernate.cfg.xml"); // Populates the properties from the cfg file
+        System.out.println(configuration.getProperties()); // Prints the properties
         SessionFactory sessionFactory = configuration.buildSessionFactory();
+        System.out.println(sessionFactory.getClass());
         Session session = sessionFactory.openSession();
+        System.out.println(session.getClass());
         Transaction transaction = session.beginTransaction();
-        Product product = new Product(1, "MotherBoard", 11000.00f,1);
+        Product product = new Product(7, "Ball", 300.00f,1);
 
         //Saving the object
+        System.out.println("Before save method");
         session.save(product);
+        System.out.println("After save method");
 
         // All non select operations should be committed
         transaction.commit();
+        System.out.println("Object saved");
 
         session.close();
         sessionFactory.close();
